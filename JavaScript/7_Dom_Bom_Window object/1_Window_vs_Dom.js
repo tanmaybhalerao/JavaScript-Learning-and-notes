3ada1dfae714c658c88b50e1bd968e90febb360d
tanmaybhalerao <68472047+tanmaybhalerao@users.noreply.github.com>
2022-08-14 23:20:23 +0530

    Dom bom and windows object

---
+++ JavaScript/7_Dom_Bom_Window object/1_Window_vs_Dom.js
@@ -0,0 +1,42 @@
+//Window vs DOM
+//================Window Object===========================
+//1. window is a global object each everything on browser
+// screen is a part of window
+
+//2.Each and everything visible on webpage is part of window object
+//example : title bar ,minimize ,exit or cancel , add new page sign ,address bar etc
+
+//3.All the member like object ,methods and propertise .If they are part
+// of the window object then we dont refer the window object
+//Example window.location or location both work in same way
+
+
+//4.Window has method, propertise and objects. settimeout and settimeinterval is method .Dom is object,  it also have screen object which describes physical height and width of the screen. window.innerHeight and window.innerWidth are the propertise of window
+
+//=================DOM===============================
+//1.DOM is a part of window object
+//in simple words we can say that windown is parent and DOM is child
+
+//2.The page visible under the address bar section  is DOM
+
+//3.DOM object always need to be used as referece to parts
+//example: document.getelementbyid etc
+//here Document is used as reference
+
+//Dom deals with html elements.
+
+
+
+
+//=====================BOM==============================
+//Bom stands for browser object model
+//it deals with browsers components like navigator
+//screen, location ,frames and history(most imps)
+
+//alert,confirm,prompt are the part of BOM
+
+
+
+
+
+/////////////////////NOTE:TRY IN CONSOLE WINDOW IN CHROME//////////////////
